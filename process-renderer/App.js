1df53082e783b553e4077a491d2310e4e9f0c000
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-04-12 14:01:00 -0400

    refactor: remove jquery

--- process-renderer/App.js
+++ process-renderer/App.js
@@ -1,3 +1,10 @@
+function hide(el) {
+	el.setAttribute('aria-visible', false);
+}
+function show(el) {
+	el.setAttribute('aria-visible', true);
+}
+
 const formatSeconds = new Intl.NumberFormat(undefined, { style: 'unit', unit: 'second', unitDisplay: 'long' }).format;
 
 window.App = class App {
@@ -19,6 +26,36 @@ window.App = class App {
 		api.on('butler:error', (...args) => {
 			this.onButlerError(...args);
 		});
+		this.init();
+	}
+	async init() {
+		document.querySelectorAll('fieldset').forEach(hide);
+		show(document.querySelector('#login'));
+
+		document.querySelectorAll('#channelWin,#channelOsx,#channelLinux,#channelOther').forEach(i => {
+			i.addEventListener('change', this.validate);
+		});
+		document.querySelector('#btnLogin').addEventListener('click', this.login);
+		document.querySelector('#btnLogout').addEventListener('click', this.logout);
+		document.querySelector('#userSelect').addEventListener('change', event => {
+			this.selectUser(event.currentTarget.value);
+		});
+		document.querySelector('#projectSelect').addEventListener('change', event => {
+			this.selectProject(event.currentTarget.value);
+			this.validate();
+		});
+		document.querySelector('#selectedFile').addEventListener('change', event => {
+			this.selectFile(event.currentTarget.files[0]);
+		});
+		document.querySelector('#btnPush').addEventListener('click', () => {
+			this.butler_push(this.selectedFile, this.getProjectUrl());
+		});
+		document.querySelector('#btnCheckStatus').addEventListener('click', () => {
+			this.butler_status(this.getProjectUrl());
+		});
+
+		await this.butler_init();
+		this.tryLogin();
 	}
 	async tryLogin() {
 		const token = await api.invoke('oauth:autologin');
@@ -26,79 +63,75 @@ window.App = class App {
 			this.login();
 		}
 	}
-	async login() {
-		$('#btnLogin').prop('disabled', true);
-		await api.invoke('butler', 'login');
-		const auth = await api.invoke('oauth:login', $('#rememberMe').prop('checked'));
+	login = async () => {
+		try {
+			document.querySelector('#btnLogin').disabled = true;
+			await api.invoke('butler', 'login');
+			const auth = await api.invoke('oauth:login', document.querySelector('#rememberMe').checked);
 
-		$.ajax({
-			url: 'https://itch.io/api/1/' + auth + '/my-games',
-		})
-			.done(
-				function (data) {
-					// error handling
+			const response = await fetch(`https://itch.io/api/1/${auth}/my-games`);
+			const data = await response.json();
+			// error handling
 
-					if (data.errors) {
-						// error when submitting API key
-						this.failLogin(data.errors.toString());
-						return;
-					}
+			if (data.errors) {
+				// error when submitting API key
+				this.failLogin(data.errors.toString());
+				return;
+			}
 
-					if (!data.games.hasOwnProperty('length') || data.games.length <= 0) {
-						// the user has nothing associated with their key
-						this.failLogin(
-							"Error: couldn't find any projects associated with this account. gui-butler only pushes builds to existing projects; you need to use itch.io or the official app to actually create them."
-						);
-						return;
-					}
+			if (!data.games.hasOwnProperty('length') || data.games.length <= 0) {
+				// the user has nothing associated with their key
+				this.failLogin(
+					"Error: couldn't find any projects associated with this account. gui-butler only pushes builds to existing projects; you need to use itch.io or the official app to actually create them."
+				);
+				return;
+			}
 
-					// find all usernames associated with key by stripping URLs
-					var users = {};
-					for (var i = 0; i < data.games.length; ++i) {
-						var username = data.games[i].url.split('https://')[1].split('.itch.io/')[0];
-						if (!users[username]) {
-							users[username] = {
-								username: username,
-								projects: [],
-							};
-						}
-						users[username].projects.push(data.games[i]);
-					}
+			// find all usernames associated with key by stripping URLs
+			const users = {};
+			for (let i = 0; i < data.games.length; ++i) {
+				const username = new URL(data.games[i].url).host.split('.itch.io')[0];
+				if (!users[username]) {
+					users[username] = {
+						username: username,
+						projects: [],
+					};
+				}
+				users[username].projects.push(data.games[i]);
+			}
 
-					// add users to internal list + dropdown
-					this.users = [];
-					var s = '';
-					var i = 0;
-					for (var u in users) {
-						this.users.push(users[u]);
-						s += '<option value="' + i++ + '"' + (i == 0 ? ' selected ' : '') + '>' + users[u].username + '</option>';
-					}
-					$('#userSelect').html(s);
+			// add users to internal list + dropdown
+			this.users = [];
+			let s = '';
+			let i = 0;
+			for (let u in users) {
+				this.users.push(users[u]);
+				s += '<option value="' + i++ + '"' + (i == 0 ? ' selected ' : '') + '>' + users[u].username + '</option>';
+			}
+			document.querySelector('#userSelect').innerHTML = s;
 
-					// number of users may have changed since last login
-					if (parseInt(window.localStorage.getItem('userIdx') || '0', 10) >= this.users.length) {
-						window.localStorage.setItem('userIdx', '0');
-					}
+			// number of users may have changed since last login
+			let selectedUser = parseInt(window.localStorage.getItem('userIdx') || '0', 10);
+			if (selectedUser >= this.users.length) {
+				selectedUser = 0;
+				window.localStorage.setItem('userIdx', selectedUser);
+			}
 
-					// update display
-					$('#login').hide();
-					$('#logout').show();
+			// update display
+			hide(document.querySelector('#login'));
+			show(document.querySelector('#logout'));
 
-					// auto-select the first user in the dropdown
-					// (provides a bit of feedback + removes the need for a null entry)
-					$('#userSelect')
-						.val(window.localStorage.getItem('userIdx') || '0')
-						.change();
+			// auto-select the first user in the dropdown
+			// (provides a bit of feedback + removes the need for a null entry)
+			document.querySelector('#userSelect').value = selectedUser;
+			this.selectUser(selectedUser);
 
-					$('#selectUser').show();
-				}.bind(this)
-			)
-			.fail(
-				function () {
-					this.failLogin('Error: itch.io failed to respond. Make sure your web API key has been entered correctly.\nAlso check your internet connection etc.');
-				}.bind(this)
-			);
-	}
+			show(document.querySelector('#selectUser'));
+		} catch (err) {
+			console.error(err);
+			this.failLogin('Error: itch.io failed to respond.');
+		}
+	};
 	failLogin(error) {
 		// display error
 		// TODO: something nicer than an alert
@@ -106,83 +139,72 @@ window.App = class App {
 
 		// make sure the login is visible and interactive
 		// so the user can change info and try again
-		$('#btnLogin').prop('disabled', false);
-		$('#login').show();
+		document.querySelector('#btnLogin').disabled = false;
+		show(document.querySelector('#login'));
 	}
-	async logout() {
-		$('#btnLogout').prop('disabled', true);
+	logout = async () => {
+		document.querySelector('#btnLogout').disabled = true;
 		await api.invoke('butler', 'logout');
 		await api.invoke('oauth:logout');
-		$('#btnLogout').prop('disabled', false);
-		$('#login').show();
+		document.querySelector('#btnLogout').disabled = false;
+		show(document.querySelector('#login'));
 
-		$('#btnLogin').prop('disabled', false);
+		document.querySelector('#btnLogin').disabled = false;
 
-		$('#logout').hide();
-		$('#selectProject').hide();
-		$('#selectBuild').hide();
-		$('#selectUser').hide();
-		$('#pushBuild').hide();
-	}
+		document.querySelectorAll('#logout,#selectProject,#selectBuild,#selectUser,#pushBuild').forEach(hide);
+	};
 	selectUser(idx) {
 		this.selectedUserIdx = parseInt(idx, 10);
 		this.selectedUser = this.users[this.selectedUserIdx];
+		window.localStorage.setItem('userIdx', this.selectedUserIdx);
 
 		// update link
-		$('#userPreview').attr('href', 'https://' + this.selectedUser.username + '.itch.io');
+		document.querySelector('#userPreview').href = `https://${this.selectedUser.username}.itch.io`;
 
 		// add the users games to the dropdown
 		this.projects = this.selectedUser.projects;
-		var s = '';
-		for (var i = 0; i < this.projects.length; ++i) {
-			var g = this.projects[i];
+		let s = '';
+		for (let i = 0; i < this.projects.length; ++i) {
+			const g = this.projects[i];
 			s += '<option value="' + i + '"' + (i == 0 ? ' selected ' : '') + '>' + g.title + '</option>';
 		}
-		$('#projectSelect').html(s);
+		document.querySelector('#projectSelect').innerHTML = s;
 
 		// number of projects may have changed since last login
-		if (parseInt(window.localStorage.getItem('projectIdx') || '0', 10) >= this.projects.length) {
-			window.localStorage.setItem('projectIdx', '0');
-		}
-
-		// save selection to localStorage
-		if ((window.localStorage.getItem('rememberMe') || '0') == '1') {
-			window.localStorage.setItem('userIdx', idx);
+		let projectIdx = parseInt(window.localStorage.getItem('projectIdx') || '0', 10);
+		if (projectIdx >= this.projects.length) {
+			projectIdx = 0;
+			window.localStorage.setItem('projectIdx', projectIdx);
 		}
 
 		// auto-select the first game in the dropdown
 		// (provides a bit of feedback + removes the need for a null entry)
-		$('#projectSelect')
-			.val(window.localStorage.getItem('projectIdx') || '0')
-			.change();
+		document.querySelector('#projectSelect').value = projectIdx;
+		this.selectProject(projectIdx);
 
-		$('#selectProject').show();
+		show(document.querySelector('#selectProject'));
 	}
 	selectProject(idx) {
 		this.selectedProjectIdx = parseInt(idx, 10);
 		this.selectedProject = this.projects[this.selectedProjectIdx];
+		window.localStorage.setItem('projectIdx', this.selectedProjectIdx);
 
 		// get rid of the old cover, then set then new one
-		$('#projectCover').attr('src', '');
-		$('#projectCover').attr('src', this.selectedProject.cover_url);
+		document.querySelector('#projectCover').src = '';
+		document.querySelector('#projectCover').src = this.selectedProject.cover_url;
 
 		// update text
-		$('#projectText').text(this.selectedProject.short_text);
+		document.querySelector('#projectText').textContent = this.selectedProject.short_text;
 
 		// update link
-		$('#projectUrl').attr('href', this.selectedProject.url);
+		document.querySelector('#projectUrl').href = this.selectedProject.url;
 
-		// save selection to localStorage
-		if (window.localStorage.getItem('rememberMe') || '0' == '1') {
-			window.localStorage.setItem('projectIdx', idx);
-		}
-
-		$('#selectBuild').show();
+		show(document.querySelector('#selectBuild'));
 	}
 	async selectFile(file) {
 		if (file) {
 			this.selectedFile = file.path;
-			$('#pushBuild').show();
+			show(document.querySelector('#pushBuild'));
 
 			// if there aren't already any channels set
 			// search the filename for "win","osx","linux" and check their boxes
@@ -194,54 +216,49 @@ window.App = class App {
 					.split(/[\/\\]+/)
 					.pop();
 				if (fileEnd.indexOf('win') !== -1) {
-					$('#channelWin').trigger('click');
+					document.querySelector('#channelWin').checked = true;
 				}
 				if (fileEnd.indexOf('osx') !== -1) {
-					$('#channelOsx').trigger('click');
+					document.querySelector('#channelOsx').checked = true;
 				}
 				if (fileEnd.indexOf('linux') !== -1) {
-					$('#channelLinux').trigger('click');
+					document.querySelector('#channelLinux').checked = true;
 				}
 				if (fileEnd.indexOf('web') !== -1) {
-					$('#channelOther').val('web-');
+					document.querySelector('#channelOther').value += 'web-';
 				}
 				if (fileEnd.indexOf('32') !== -1) {
-					$('#channelOther').val($('#channelOther').val() + '32-');
+					document.querySelector('#channelOther').value += '32-';
 				}
 				if (fileEnd.indexOf('64') !== -1) {
-					$('#channelOther').val($('#channelOther').val() + '64-');
+					document.querySelector('#channelOther').value += '64-';
 				}
-				if ($('#channelOther').val().substr(-1) == '-') {
-					$('#channelOther').val($('#channelOther').val().slice(0, -1));
-					$('#channelOther').trigger('change');
-				}
+				this.validate();
 			}
 		} else {
 			// canceled selection
-			$('#pushBuild').hide();
+			hide(document.querySelector('#pushBuild'));
 		}
 	}
 	getChannels() {
-		var channels = '';
+		let channels = '';
 
 		// add preset channels
-		if ($('#channelWin')[0].checked) {
+		if (document.querySelector('#channelWin').checked) {
 			channels += 'win-';
 		}
-		if ($('#channelOsx')[0].checked) {
+		if (document.querySelector('#channelOsx').checked) {
 			channels += 'osx-';
 		}
-		if ($('#channelLinux')[0].checked) {
+		if (document.querySelector('#channelLinux').checked) {
 			channels += 'linux-';
 		}
 
 		// add custom channels
-		channels += $('#channelOther').val().toLowerCase();
+		channels += document.querySelector('#channelOther').value.toLowerCase();
 
-		// remove the last dash
-		if (channels.substr(-1) == '-') {
-			channels = channels.slice(0, -1);
-		}
+		// remove trailing dash
+		channels = channels.replace(/-$/, '');
 
 		return channels;
 	}
@@ -251,51 +268,46 @@ window.App = class App {
 	}
 	getProjectUrl() {
 		// butler URLs are user/project-url:channels-in-a-dash-separated-list
-		return this.selectedUser.username + '/' + this.getProjectShortUrl() + ':' + this.getChannels();
+		return `${this.selectedUser.username}/${this.getProjectShortUrl()}:${this.getChannels()}`;
 	}
-	validate() {
-		var s = this.getProjectUrl();
-		$('#targetPreview').val(s);
+	validate = () => {
+		document.querySelector('#targetPreview').value = this.getProjectUrl();
 
-		if (this.getChannels() <= 0) {
-			$('#btnPush').prop('disabled', true);
-			$('#btnCheckStatus').prop('disabled', true);
+		if (this.getChannels().length <= 0) {
+			document.querySelector('#btnPush').disabled = true;
+			document.querySelector('#btnCheckStatus').disabled = true;
 		} else {
-			$('#btnPush').prop('disabled', false);
-			$('#btnCheckStatus').prop('disabled', false);
+			document.querySelector('#btnPush').disabled = false;
+			document.querySelector('#btnCheckStatus').disabled = false;
 		}
-	}
+	};
 	// calling butler
 	async butler_init() {
 		await api.invoke('butler', 'upgrade');
 		const version = await api.invoke('butler', 'version');
-		$('#version').html(version.value.version);
+		document.querySelector('#version').textContent = version.value.version;
 	}
 	async butler_push(file, url) {
-		$('#progress').show();
-		$('#progress')[0].scrollIntoView();
+		show(document.querySelector('#progress'));
+		document.querySelector('#progress').scrollIntoView();
 		await api.invoke('butler', 'push', file, url);
-		$('#progress').hide();
+		hide(document.querySelector('#progress'));
 	}
 	async butler_status(url) {
 		await api.invoke('butler', 'status', url);
-	}
-	// receiving messages from butler
-	async onMessage(data) {
-		await this['json_' + data.type](data);
 	}
 	onButlerLog(message) {
 		// general purpose logging
 		// TODO: provide a way to show debug logs in addition to info
 		if (message.level == 'info') {
-			$('#output').append(message.message + '\n');
-			$('#output').scrollTop($('#output')[0].scrollHeight);
+			document.querySelector('#output').append(message.message + '\n');
+			document.querySelector('#output').scrollTop = document.querySelector('#output').scrollHeight;
 		}
 	}
 	onButlerProgress(message) {
 		// update on upload progress
-		$('progress').val(message.progress);
-		$('#eta').val(formatSeconds(message.eta));
+		document.querySelector('#progress').value = message.progress;
+		document.querySelector('#eta').value = formatSeconds(message.eta);
 	}
 	onButlerError(message) {
 		console.error(message);
