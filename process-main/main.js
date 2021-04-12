7fa5b93f8db79832db53a9d2d82f77ba60e7aebb
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-04-12 00:57:43 -0400

    fix: single instance lock

--- process-main/main.js
+++ process-main/main.js
@@ -2,6 +2,12 @@ const { app, session } = require('electron');
 require('./ipc');
 
 const { createWindow } = require('./window');
+const primaryInstance = app.requestSingleInstanceLock();
+if (!primaryInstance) {
+	app.requestSingleInstanceLock();
+	return;
+}
+
 app.on('ready', () => {
 	// disable all session permissions
 	session.defaultSession.setPermissionCheckHandler((_webContents, _permission, callback) => {
